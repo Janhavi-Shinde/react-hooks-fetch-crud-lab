580440276a16edb2da3d93a0a0efba560bec099f
janhavi <janashinde394@gmail.com>
2022-04-11 16:50:23 +1000

    starting to refactor

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem() {
+function QuestionItem({q, handleDelete}) {
   // console.log(question); // array of 6 objects
   // const [ one , two, three, four, five, six] = question
   // console.log(one);
@@ -12,12 +12,36 @@ function QuestionItem() {
   // const idB = question.map(q => q.id);
   // const promptB = question.map(q => q.prompt);
   // const correctIndexB = question.map(q => q.correctIndex);
-
-
-
+  
+  // function handlePatch(){
+  //   fetch(`http://localhost:4000/questions/${id}`, {
+  //     method: 'PATCH',
+  //     headers: {'Content-Type': 'application/JSON'},
+  //     body: {
+  //       "correctIndex": integer
+  //     } 
+  //   })
+  // }
   return (
     <> 
-    {/* {list} */}
+    <h4>Question {q.id}</h4>
+    
+    <h5>Prompt: {q.prompt}</h5>
+    
+    <label htmlFor="selectAnswers">
+      Correct Answer:
+    </label>
+    
+    <select name="selectAnswers"  defaultValue={q.correctIndex} >
+      {q.answers.map((answer, index) => (
+        <option key={index} value={index}>
+          {answer}
+        </option>
+      ))}
+    </select>
+    <br></br>
+    
+    <button onClick={() => handleDelete(q.id)}>Delete Question</button>
     </>)
 }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import QuestionItem from './QuestionItem';
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
   const [question, setQuestion] = useState(null);
@@ -9,46 +9,45 @@ function QuestionList() {
       .then(r => {
         console.log(r); //ok is true
         if (!r.ok) { throw Error('could not fetch data for that resource') }
-        return r.json() })
-      .then((data) => {setQuestion(data)}) // returning undefined
+        return r.json()
+      })
+      .then((data) => { setQuestion(data) }) // returning undefined
       .catch(err => console.log(err.message));
-      // console.log(data);
-      // console.log(question);
+    // console.log(data);
+    // console.log(question);
   }, []);
 
-  // function handleDelete (id) { 
-  //   fetch('http://localhost:4000/questions/' + id, {
-  //     method: 'DELETE'
-  //   }).then(()=> {console.log("delete request sent")})
-  // }
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE'
+    }).then(() => { console.log(`delete request sent for id ${id}`) })
+  }
+
  
+  // {
+  //   fetch(`http://localhost:4000/questions/${id}`, {
+  //     method: "DELETE",
+  //   })
+  //     .then((r) => r.json())
+  //     .then(() => {
+  //       const updatedQuestions = questions.filter((q) => q.id !== id);
+  //       setQuestions(updatedQuestions);
+  //     });
+  // }
+  const list = question && question.map(q => (
+    <li key={q.id}>
+      <QuestionItem q={q}  handleDelete={handleDelete}/>
+    </li>)
+  )
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul> 
-         { question && question.map(q => (
-      <li key={q.id}>
-        <h4>Question {q.id}</h4>
-        
-        <h5>Prompt: {q.prompt}</h5>
-  
-        <label>
-          Correct Answer:
-          <select defaultValue={q.correctIndex}> 
-            {q.answers.map((answer, index) => (
-              <option key={index} value={index}>
-                {answer}
-              </option>
-            )) }
-          </select>
-        </label>
-  
-        <button >Delete Question</button>
-      </li>)
-    ) }
-         </ul> 
+      <ul>
+        {list}
+      </ul>
     </section>
-  );
+  )
+  ;
 }
 
 export default QuestionList;
